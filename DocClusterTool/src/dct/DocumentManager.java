596d7a2e8213c0aee2646a31ca1de816c6911537
Marc Bouvier <marcrbouvier@gmail.com>
2013-11-24 13:26:10 -0600

    Convert Cranfield Collection to ARFF
    This is done. There are three attributes in the Cranfield ARFF data
    file:
    1. filename
    2. contents
    3. class (All documents have the same class)
    All tags and punctuation have been removed.

--- DocClusterTool/src/dct/DocumentManager.java
+++ DocClusterTool/src/dct/DocumentManager.java
@@ -13,94 +13,97 @@ import weka.core.converters.ArffSaver;
 import weka.core.tokenizers.WordTokenizer;
 
 public class DocumentManager {
-    public DocumentManager() {}
+	public DocumentManager() {}
 
-    private boolean isTag(String word) {
-        if(word.charAt(0) == '<' && word.charAt(word.length() - 1) == '>')
-            return true;
-        return false;
-    }
+	private boolean isTag(String word) {
+		if(word.charAt(0) == '<' && word.charAt(word.length() - 1) == '>')
+			return true;
+		return false;
+	}
 
-    private String cleanLine(String dirty) {
-        String noise = "`~@#$%^&*=+\\|;:\'\",./";
-        String clean = dirty;
+	private String cleanLine(String dirty) {
+		String noise = "`~@#$%^&*=+\\|;:\'\",./";
+		String clean = dirty;
 
-        for(int i = 0; i < noise.length(); i++) {
-            clean = clean.replace(noise.charAt(i) + "", "");
-        }
+		for(int i = 0; i < noise.length(); i++) {
+			clean = clean.replace(noise.charAt(i) + "", "");
+		}
 
-        return clean;
-    }
+		return clean;
+	}
 
-    public void createCranDataset(String dataDir) throws IOException {
-        File inDir = new File(dataDir);
-        ArffSaver saver = new ArffSaver();
-        saver.setFile(new File(dataDir + "/arff/cranfield.arff"));
+	public void createCranDataset(String dataDir, String cranDir)
+			throws IOException {
+		File inDir = new File(dataDir + cranDir);
+		ArffSaver saver = new ArffSaver();
+		saver.setFile(new File(dataDir + "arff/cranfield.arff"));
 
-        Attribute filename = new Attribute("filename");
-        Attribute contents = new Attribute("contents");
+		Attribute filename = new Attribute("filename", (FastVector)null);
+		Attribute contents = new Attribute("contents", (FastVector)null);
 
-        FastVector groupnv = new FastVector(1);
-        groupnv.addElement("cranfield");
-        Attribute group = new Attribute("class", groupnv);
+		FastVector groupnv = new FastVector(1);
+		groupnv.addElement("cranfield");
+		Attribute group = new Attribute("class", (FastVector)null);
 
-        FastVector attrs = new FastVector(3);
-        attrs.addElement(filename);
-        attrs.addElement(contents);
-        attrs.addElement(group);
+		FastVector attrs = new FastVector(3);
+		attrs.addElement(filename);
+		attrs.addElement(contents);
+		attrs.addElement(group);
 
-        Instances dataset = new Instances("cranfield", attrs, 0);
+		Instances data = new Instances("cranfield", attrs, 0);
+		data.setClass(group);
 
-        String delimiters = " \r\n\t.,;:\'\"()[]{}?!-_";
+		String delimiters = " \r\n\t.,;:\'\"()[]{}?!-_";
 
-        WordTokenizer tokenizer = new WordTokenizer();
-        tokenizer.setDelimiters(delimiters);
+		WordTokenizer tokenizer = new WordTokenizer();
+		tokenizer.setDelimiters(delimiters);
 
-        if(inDir.isDirectory()) {
-            String files[] = inDir.list();
+		if(inDir.isDirectory()) {
+			String files[] = inDir.list();
 
-            for(int i = 0; i < files.length; i++) {
-                File file = new File(inDir + "/" + files[i]);
-                System.out.println(files[i]);
+			for(int i = 0; i < files.length; i++) {
+				File file = new File(inDir + "/" + files[i]);
 
-                if(!file.isDirectory()) {
-                    BufferedReader reader = new BufferedReader(new FileReader(
-                            file));
-                    Instance inst = new Instance(2);
-                    inst.setValue((Attribute)attrs.elementAt(0), files[i]);
+				if(!file.isDirectory()) {
+					BufferedReader reader = new BufferedReader(new FileReader(
+							file));
+					Instance inst = new Instance(3);
+					inst.setValue((Attribute)attrs.elementAt(0), files[i]);
 
-                    String rawLine = "";
-                    String line = "";
+					String rawLine = "";
+					String line = "";
 
-                    while((rawLine = reader.readLine()) != null) {
-                        rawLine = cleanLine(rawLine);
-                        tokenizer.tokenize(rawLine);
+					while((rawLine = reader.readLine()) != null) {
+						rawLine = cleanLine(rawLine);
+						tokenizer.tokenize(rawLine);
 
-                        while(tokenizer.hasMoreElements()) {
-                            String token = tokenizer.nextElement().toString();
-                            if(!isTag(token))
-                                line += token + ' ';
-                        }
-                    }
-                    inst.setValue((Attribute)attrs.elementAt(1), line);
-                    inst.setValue((Attribute)attrs.elementAt(2), "cranfield");
-                    dataset.add(inst);
-                    reader.close();
-                }
-            }
-            saver.writeBatch();
-        }
-        else {
-            System.out.println(dataDir + " is not a directory.");
-        }
-    }
+						while(tokenizer.hasMoreElements()) {
+							String token = tokenizer.nextElement().toString();
+							if(!isTag(token))
+								line += token + ' ';
+						}
+					}
+					inst.setValue((Attribute)attrs.elementAt(1), line);
+					inst.setValue((Attribute)attrs.elementAt(2), "cranfield");
+					data.add(inst);
+					reader.close();
+				}
+			}
+			saver.setInstances(data);
+			saver.writeBatch();
+		}
+		else {
+			System.out.println(dataDir + " is not a directory.");
+		}
+	}
 
-    public static void main(String[] args) throws IOException {
-        String cranDir = "/home/marc/data/cranfield";
-        String news20Dir = "/home/marc/data/20news-18828";
-        String reutersDir = "/home/marc/data/reuters21578";
+	public static void main(String[] args) throws IOException {
+		String dataDir = "/home/marc/data/";
+		String cranDir = "cranfield";
+		String news20Dir = "20news-18828";
+		String reutersDir = "reuters21578";
 
-        DocumentManager docMan = new DocumentManager();
-        docMan.createCranDataset(cranDir);
-    }
+		DocumentManager docMan = new DocumentManager();
+		docMan.createCranDataset(dataDir, cranDir);
+	}
 }
