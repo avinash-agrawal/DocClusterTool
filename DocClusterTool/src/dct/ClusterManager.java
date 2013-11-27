8952f137385fc361ec1cab42e79afc97ac91f4f4
Marc Bouvier <marcrbouvier@gmail.com>
2013-11-27 06:10:33 -0600

    Add 2D Graph Popup After Clustering is Finished

--- DocClusterTool/src/dct/ClusterManager.java
+++ DocClusterTool/src/dct/ClusterManager.java
@@ -1,12 +1,16 @@
 package dct;
 
 import java.awt.BorderLayout;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.File;
-import java.io.IOException;
+
+import javax.swing.JFrame;
 
 import weka.clusterers.ClusterEvaluation;
 import weka.clusterers.HierarchicalClusterer;
 import weka.core.Instances;
+import weka.core.Utils;
 import weka.core.converters.ArffLoader;
 import weka.gui.explorer.ClustererPanel;
 import weka.gui.visualize.PlotData2D;
@@ -16,7 +20,7 @@ public class ClusterManager {
 	public ClusterManager() {}
 
 	private void clusterCollection(String dataFile, String[] options)
-			throws IOException, Exception {
+			throws Exception {
 		ArffLoader loader = new ArffLoader();
 		loader.setFile(new File(dataFile));
 
@@ -32,32 +36,31 @@ public class ClusterManager {
 
 		System.out.println(eval.clusterResultsToString());
 
-		PlotData2D plotData =
-				ClustererPanel.setUpVisualizableInstances(data, eval);
+		PlotData2D plotData = ClustererPanel.setUpVisualizableInstances(data,
+				eval);
 		plotData.setPlotName(data.relationName());
 
 		VisualizePanel vp = new VisualizePanel();
-		vp.setName("(" + data.relationName() + ")");
-		plotData.setPlotName("(" + data.relationName() + ")");
+		vp.setName(data.relationName());
+		plotData.setPlotName(data.relationName());
 		vp.addPlot(plotData);
 
 		String plotName = vp.getName();
-		final javax.swing.JFrame jf =
-				new javax.swing.JFrame("Weka Clusterer Visualize: " + plotName);
+		final JFrame jf = new JFrame("Weka Clusterer Visualize: " + plotName);
 		jf.setSize(1024, 768);
 		jf.getContentPane().setLayout(new BorderLayout());
 		jf.getContentPane().add(vp, BorderLayout.CENTER);
-		jf.addWindowListener(new java.awt.event.WindowAdapter() {
-			public void windowClosing(java.awt.event.WindowEvent e) {
+		jf.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
 				jf.dispose();
 			}
 		});
 		jf.setVisible(true);
 	}
 
-	public static void main(String[] args) throws IOException, Exception {
+	public static void main(String[] args) throws Exception {
 		String dataFile = "/home/marc/data/arff/cranfield_wv.arff";
-		String[] options = weka.core.Utils.splitOptions("-N 10 -L COMPLETE");
+		String[] options = Utils.splitOptions("-N 10 -L COMPLETE");
 		ClusterManager clustMan = new ClusterManager();
 		clustMan.clusterCollection(dataFile, options);
 	}
